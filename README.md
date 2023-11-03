6dccbaf528b82d6b728ee58cbd3fdc606ec9d6ad
Emily Pearl <Emilypearl9193@gmail.com>
2023-11-02 19:38:15 -0600

    toggle one

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 ### Attributions
 - blog page from cpnt260-final assignment
 - videos and images by my grandfather Bob Stewart
-- for manipulating css with javascript [Fabio Musanni](https://www.youtube.com/watch?v=ih44U6EWyOs)
+- for manipulating css with javascript [Fabio Musanni](https://www.youtube.com/watch?v=ih44U6EWyOs) and [w3schools](https://www.w3schools.com/js/js_htmldom_css.asp)
 
--- js/client.js
+++ js/client.js
@@ -4,10 +4,23 @@ let textColor = document.querySelector('#textColorOne','#textColorTwo','#textCol
 //console log to check 
 console.log(textColor);
 //change color of h3
-textColor.computedStyleMap.color = 'orange';
+document.getElementById('textColor').style.backgroundColor = 'orange';
 //could not fix bug
 
 //rendering cards
 let card = ['#cardOne','#cardTwo','cardThree'];
 //log to check
 console.log(card);
+
+//toggle one
+let nav = document.querySelectorAll('.navigation');
+
+console.log(nav);
+
+nav.forEach('navigation')
+  nav.addEventListener('onclick')
+    if (nav = true)
+      document.getElementById('navigation').style.backgroundColor = 'blue';
+    
+    else if (nav = false)
+    document.getElementById('navigation').style.backgroundColor = 'black';
